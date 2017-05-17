1af689783e1f19afe54c81ee2450b7b19a00b2fe
Joe Davis <joedavis@HDC-WRK-M51.hsd1.wa.comcast.net>
2017-05-16 20:53:58 -0700

    insert to bigquery

--- src/handlers/post-major-alerts/index.js
+++ src/handlers/post-major-alerts/index.js
@@ -1,11 +1,23 @@
 exports.postMajorAlert = function postMajorAlert (req, res) {
+  let bigQuery = require('@google-cloud/bigquery');
   if (req.body === undefined) {
     // This is an error case, as "message" is required
     res.status(400).send('No message defined!');
   } else {
-    // Everything is ok
-    //console.log(req.body.message);
-    res.body = req.body;
-    res.status(200).end();
+    let projectId = "quotecenter-dev";
+    let datasetId = "Monitors_Alerts";
+    let tableId = "all_newrelic_alerts";
+    let bq = bigQuery({projectId: projectId});
+    
+    bq.dataset(datasetId)
+      .table(tableId)
+      .insert(req.body)
+      .then((insertErrors) => {
+        if(insertErrors) res.status(500).end();
+        
+        res.status(200).end();
+      })
+      .catch((err) => {res.status(500).end();})
   }
 };
+

a3a420b192a121f773739e1d62fc76a67f1054bc
Joe Davis <joedavis@HDC-WRK-M51.local>
2017-05-17 07:53:42 -0700

    Added PubSub topic post

--- src/handlers/post-major-alerts/index.js
+++ src/handlers/post-major-alerts/index.js
@@ -1,13 +1,15 @@
 require('@google-cloud/debug-agent').start();
 const bigQuery = require('@google-cloud/bigquery');
+const pubSub = require('@google-cloud/pubsub');
 const Logging = require('./errors');
 
-exports.postMajorAlert = function postMajorAlert (req, res) {
+const projectId = "quotecenter-dev";
+
+exports.postMajorAlerttoBigQuery = function postMajorAlert (req, res) {
   if (req.body === undefined) {
     // This is an error case, as "message" is required
     res.status(400).send('No message defined!');
   } else {
-    let projectId = "quotecenter-dev";
     let datasetId = "Monitors_Alerts";
     let tableId = "raw_alerts";
     let bq = bigQuery({projectId: projectId});
@@ -27,6 +29,25 @@ exports.postMajorAlert = function postMajorAlert (req, res) {
         Logging.logError(err, req, res, null, (err) => {
           res.status(500).end();
         })
+      });
+  }
+};
+
+
+exports.postMajorAlert = function postMajorAlert (req, res) {
+  if (req.body === undefined) {
+    // This is an error case, as "message" is required
+    res.status(400).send('No message defined!');
+  } else {
+    let ps = pubSub({projectId: projectId});
+    let topicName = 'monitors.alerts.newrelic.major';
+
+    ps.topic(topicName)
+      .publish({'data': JSON.stringify(req.body)}, (err) => {
+        if(err) {
+          Logging.logError(err, req, res, null, ()=>{});
+        }
+
       });
   }
 };

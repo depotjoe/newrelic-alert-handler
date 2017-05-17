cb2065d087793b42ced297926445cba695df34eb
Joe Davis <joedavis@HDC-WRK-M51.hsd1.wa.comcast.net>
2017-05-16 20:25:36 -0700

    Updated function to remove reference to req.body.message

--- src/handlers/post-major-alerts/index.js
+++ src/handlers/post-major-alerts/index.js
@@ -1,5 +1,5 @@
 exports.postMajorAlert = function postMajorAlert (req, res) {
-  if (req.body.message === undefined) {
+  if (req.body === undefined) {
     // This is an error case, as "message" is required
     res.status(400).send('No message defined!');
   } else {
